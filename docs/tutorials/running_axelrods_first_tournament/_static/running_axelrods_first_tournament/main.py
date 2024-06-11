2b4250c77fc588fead6842b8cc51ad025c199cfe
Ian Miller <46457417+miller-ian@users.noreply.github.com>
2024-06-11 05:42:33 -0400

    Moved seed to Tourney arg in main.py (#1442)
    * moved seed to tourney arg in hello world docs
    
    * formatting
    
    ---------
    
    Co-authored-by: Ian Miller <ianmiller@Ians-Mac.local>

--- docs/tutorials/running_axelrods_first_tournament/_static/running_axelrods_first_tournament/main.py
+++ docs/tutorials/running_axelrods_first_tournament/_static/running_axelrods_first_tournament/main.py
@@ -11,11 +11,11 @@ first_tournament_participants_ordered_by_reported_rank = [
 number_of_strategies = len(
     first_tournament_participants_ordered_by_reported_rank
 )
-axl.seed(0)
 tournament = axl.Tournament(
     players=first_tournament_participants_ordered_by_reported_rank,
     turns=200,
     repetitions=5,
+    seed=0,
 )
 results = tournament.play()
 
